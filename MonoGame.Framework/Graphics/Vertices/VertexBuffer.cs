MerjTek/MonoGame

commit 646f20ff870fa5fb9fe266e2d26a41ca070cdb71

Add Span<T> overloads to VertexBuffer.SetData<T> (#9359)
Fixes #9316

### Description of Change

Implements Span<T> override of `VertexBuffer.SetData<T>` requested in
#9316. Makes the following assumptions about Span usage:

- If the caller is using a `Span<T>`, they want to replace a contiguous
block of vertex objects in the buffer, with no padding between elements.
They do not want to push partial updates (updating just `position` or
just `texture`). Generating `Span<T>`s of partial elements from a source
vertex array will require the allocation Span usage is trying to avoid
as you will need to first generate and populate an array of partial
elements.
- If the caller is using a `Span<T>` they intend to copy all elements in
the Span.

The new PlatformSetData<T> call for non native platforms ~~throws
`NotImplementedException` since the feature request only targeted native
platforms.~~ `PlatformNotSupportedException` as non Native platforms
still target C# 5.0 and span was introduced in C# 7.2.

### Testing

Adds new test cases to VertexBufferTests

* `dotnet test Tests\MonoGame.Tests.WindowsDX12.csproj --filter
"FullyQualifiedName~VertexBufferTest"`
* `dotnet test Tests\MonoGame.Tests.DesktopVK.csproj --filter
"FullyQualifiedName~VertexBufferTest"`


### Contributor Declaration

- [x] I certify that no LLM's were used to generate any code or
documentation in this contribution.

---------

Co-authored-by: Tom Spilman <tom@sickheadgames.com>
Co-authored-by: Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>

MonoGame.Framework/Graphics/Vertices/VertexBuffer.cs
@@ -196,11 +196,11 @@ namespace Microsoft.Xna.Framework.Graphics
         /// Vector2[] texCoords = new Vector2[numVertices];
         /// vertexBuffer.SetData(12, texCoords, 0, numVertices, vertexBuffer.VertexDeclaration.VertexStride);
         /// </code>
-        /// </remarks>
-        /// <remarks>
+        /// <para>
         /// If you provide a <c>byte[]</c> in the <paramref name="data"/> parameter, then you should almost certainly
         /// set <paramref name="vertexStride"/> to <c>1</c>, to avoid leaving any padding between the <c>byte</c> values
         /// when they are copied into the vertex buffer.
+        /// </para>
         /// </remarks>
         public void SetData<T>(int offsetInBytes, T[] data, int startIndex, int elementCount, int vertexStride) where T : struct
         {
@@ -220,7 +220,7 @@ namespace Microsoft.Xna.Framework.Graphics
         /// <param name="elementCount">Number of elements to copy from <paramref name="data"/>.
         /// The combination of <paramref name="startIndex"/> and <paramref name="elementCount"/>
         /// must be within the <paramref name="data"/> array bounds.</param>
-		public void SetData<T>(T[] data, int startIndex, int elementCount) where T : struct
+        public void SetData<T>(T[] data, int startIndex, int elementCount) where T : struct
         {
             var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
             SetDataInternal<T>(0, data, startIndex, elementCount, elementSizeInBytes, SetDataOptions.None);
@@ -233,6 +233,7 @@ namespace Microsoft.Xna.Framework.Graphics
         /// </summary>
         /// <typeparam name="T">Type of elements in the data array.</typeparam>
         /// <param name="data">Data array to be passed to the shader.</param>
+        /// <inheritdoc cref="SetData{T}(int, T[], int, int, int)" path="/remarks"/>
         public void SetData<T>(T[] data) where T : struct
         {
             var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
@@ -264,5 +265,73 @@ namespace Microsoft.Xna.Framework.Graphics
 
             PlatformSetData<T>(offsetInBytes, data, startIndex, elementCount, vertexStride, options, bufferSize, elementSizeInBytes);
         }
+
+#if NATIVE
+        /// <summary>
+        /// Sets the vertex buffer data, uses a Span including only relevant data to be copied rather than the full source array,
+        /// and the first index in the buffer to start copying to. Assumes the full Span will be copied with no padding between elements.
+        /// </summary>
+        /// <typeparam name="T">Type of elements in the data Span.</typeparam>
+        /// <param name="destinationStartIndex">The first index in the destination buffer you want to copy data to</param>
+        /// <param name="data">Data array to be passed to the shader as a Span.</param>
+        /// elementCount will be inferred to be the number of elements in <paramref name="data"/>
+        /// since the Span should only contain the relevant data to be copied.
+        /// <remarks>
+        /// If <c>T</c> is <see cref="VertexPositionTexture"/>, and you want to only update the first 10 elements of your array of
+        /// <see cref="VertexPositionTexture"/>s, you would generate a Span containing those elements and pass it in
+        /// <code>
+        /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 0, 10);
+        /// vertexBuffer.SetData(0, vptSpan);
+        /// </code>
+        /// 
+        /// If you wanted to update the next 10 elements (indicies 10-19) in the source array, you would simply update the start index
+        /// <code>
+        /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 10, 10);
+        /// vertexBuffer.SetData(10, vptSpan);
+        /// </code>
+        /// <para>
+        /// Since a Span is a wrapper around a contiguous region of arbitrary memory, this is intended for cases with a 
+        /// vertexStride of <c>sizeof(T)</c>, as you need to generate a contiguous array of only relevant elements to populate
+        /// the Span, and the extra allocation and pre-processing to generate the Span partial objects will likely outweigh 
+        /// any benefits of passing a Span instead of a copy of the source data array.
+        /// </para>
+        /// </remarks>
+        public void SetData<T>(int destinationStartIndex, Span<T> data) where T : struct
+        {
+            SetDataInternal<T>(destinationStartIndex, data, data.Length, SetDataOptions.None);
+        }
+
+        /// <summary>
+        /// Sets the vertex buffer data. This is the same as calling <see cref="SetData{T}(int, Span{T})"/>
+        /// with <c>destinationStartIndex</c> equal to <c>0</c>
+        /// </summary>
+        /// <typeparam name="T">Type of elements in the data array.</typeparam>
+        /// <param name="data">Data Span to be passed to the shader.</param>
+        /// <inheritdoc cref="SetData{T}(int, Span{T})" path="/remarks"/>
+        public void SetData<T>(Span<T> data) where T : struct
+        {
+            var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
+            SetDataInternal<T>(0, data, data.Length, SetDataOptions.None);
+        }
+
+        /// <summary/>
+        protected void SetDataInternal<T>(int destinationStartIndex, Span<T> data, int elementCount, SetDataOptions options) where T : struct
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
+            var offsetInBytes = destinationStartIndex * elementSizeInBytes;
+            var bufferSize = VertexCount * VertexDeclaration.VertexStride;
+
+            if (elementCount > data.Length || elementCount <= 0)
+                throw new ArgumentOutOfRangeException("data", "The array specified in the data parameter is not the correct size for the amount of data requested.");
+            if (offsetInBytes + elementCount * VertexDeclaration.VertexStride > bufferSize)
+                throw new ArgumentOutOfRangeException("The provided offset and data Span must total to a larger number of bytes than the vertex buffer");
+
+            PlatformSetData<T>(offsetInBytes, data, elementCount, VertexDeclaration.VertexStride, options, bufferSize, elementSizeInBytes);
+        }
+#endif
+
     }
 }

MonoGame.Framework/Platform/Native/VertexBuffer.Native.cs
@@ -50,6 +50,24 @@ public partial class VertexBuffer
         dataHandle.Free();
     }
 
+    private unsafe void PlatformSetData<T>(int offsetInBytes, Span<T> data, int elementCount, int vertexStride, SetDataOptions options, int bufferSize, int elementSizeInBytes)
+    {
+        var dataBytes = elementCount * elementSizeInBytes;
+
+        // TODO: We need to figure out the correct behavior 
+        // for SetDataOptions.None on a dynamic buffer.
+        //
+        // For now we always discard as it is a pretty safe default.
+        //
+        fixed (void* ptr = &data[0])
+        {
+            var discard = _isDynamic && options != SetDataOptions.NoOverwrite;
+
+            var dataPtr = (byte*)ptr;
+            MGG.Buffer_SetData(GraphicsDevice.Handle, ref Handle, offsetInBytes, dataPtr, elementCount, vertexStride, elementSizeInBytes, discard);
+        }
+    }
+
     private unsafe void PlatformGraphicsDeviceResetting()
     {
         if (Handle != null)

MonoGame.Framework/Platform/Graphics/Vertices/VertexBuffer.Web.cs
@@ -40,6 +40,18 @@ namespace Microsoft.Xna.Framework.Graphics
             throw new NotImplementedException();
         }
 
+        private void PlatformSetDataInternal<T>(
+            int offsetInBytes,
+            Span<T> data,
+            int elementCount,
+            int vertexStride,
+            SetDataOptions options,
+            int bufferSize,
+            int elementSizeInBytes)
+        {
+            throw new NotImplementedException();
+        }
+
         private void PlatformGraphicsDeviceResetting()
         {
             throw new NotImplementedException();

Tests/Framework/Graphics/VertexBufferTest.cs
@@ -22,6 +22,7 @@ namespace MonoGame.Tests.Graphics
             new VertexPositionTexture(new Vector3(7,8,9), new Vector2(0.5f,0.6f)),
             new VertexPositionTexture(new Vector3(10,11,12), new Vector2(0.7f,0.8f))
         };
+        public Span<VertexPositionTexture> savedDataAsSpan => savedData.AsSpan();
         VertexPositionTexture vertexZero = new VertexPositionTexture(Vector3.Zero, Vector2.Zero);
         
         [Test]
@@ -291,6 +292,70 @@ namespace MonoGame.Tests.Graphics
 
             vertexBuffer.Dispose();
         }
+
+#if VULKAN || DIRECTX12
+        [Test]
+        //[TestCase(true)]
+        [TestCase(false, 0, 4, true, null)]
+        [TestCase(false, 1, 3, true, null)]
+        [TestCase(false, 1, 2, true, null)]
+        [TestCase(false, 4, 1, false, typeof(ArgumentOutOfRangeException))]
+        [RunOnUI]
+        public void SetDataStructWithSpan(bool dynamic, int destinationStartIndex, int elementCount, bool shouldSucceed, Type expectedExceptionType)
+        {
+            const int size = 4;
+            var testData = new VertexPositionTexture[size];
+            for (var i = 0; i < size; i++)
+            {
+                testData[i] = new VertexPositionTexture(
+                    new Vector3(i * 3, i * 3 + 1, i * 3 + 2),
+                    new Vector2(i * 2 / (float)10, (i * 2 + 1) / (float)10));
+            }
+
+            var vertexBuffer = (dynamic)
+                ? new DynamicVertexBuffer(gd, typeof(VertexPositionTexture), savedData.Length,
+                    BufferUsage.None)
+                : new VertexBuffer(gd, typeof(VertexPositionTexture), savedData.Length,
+                    BufferUsage.None);
+            var dataSpan = new Span<VertexPositionTexture>();
+            if (shouldSucceed)
+            { 
+                dataSpan = new Span<VertexPositionTexture>(testData, destinationStartIndex, elementCount);
+            }
+            else
+            {
+                dataSpan = new Span<VertexPositionTexture>(testData);
+            }
+
+            var vertexStride = VertexPositionTexture.VertexDeclaration.VertexStride;
+
+            // initialize data with standard call
+            vertexBuffer.SetData(savedData);
+
+            if (!shouldSucceed)
+                Assert.Throws(expectedExceptionType, () => vertexBuffer.SetData(destinationStartIndex, savedDataAsSpan));
+            else
+            {
+                // initialize with standard call
+                vertexBuffer.SetData(destinationStartIndex, dataSpan);
+
+                var readData = new VertexPositionTexture[savedData.Length];
+                vertexBuffer.GetData(0, readData, 0, savedData.Length, vertexStride);
+                Assert.AreEqual(
+                    dataSpan.ToArray(),
+                    readData.Take(destinationStartIndex..(destinationStartIndex + elementCount)).ToArray());
+                for(int i = 0; i < savedData.Length; i++)
+                {
+                    if (i < destinationStartIndex || i >= destinationStartIndex + elementCount)
+                    {
+                        Assert.AreEqual(savedData[i], readData[i]);
+                    }
+                }
+            }
+
+            vertexBuffer.Dispose();
+        }
+#endif
 
         [Test]
         //[TestCase(true)]

commit 5cebc1e12a2c57f789f37e863f4edb1c65280bf3

Fix: Code cleanup from #9359, move Native only overrides to .Native class (#9512)
Fixes minor code quality issue introduced in #9359

### Description of Change

`VertexBuffer.SetData` overrides that use `Span<T>` were added in #9359.
They were added to the platform agnostic `VertexBuffer.cs` class behind
a preprocessor directive as that looked to be where to put all public
methods/expose APIs. Instead, the methods should be placed in the Native
platform specific `VertexBuffer.Native.cs` class and not bloat the
common class with platform specific APIs


### Contributor Declaration

- [x] I certify that no LLM's were used to generate any code or
documentation in this contribution.

MonoGame.Framework/Graphics/Vertices/VertexBuffer.cs
@@ -265,73 +265,5 @@ namespace Microsoft.Xna.Framework.Graphics
 
             PlatformSetData<T>(offsetInBytes, data, startIndex, elementCount, vertexStride, options, bufferSize, elementSizeInBytes);
         }
-
-#if NATIVE
-        /// <summary>
-        /// Sets the vertex buffer data, uses a Span including only relevant data to be copied rather than the full source array,
-        /// and the first index in the buffer to start copying to. Assumes the full Span will be copied with no padding between elements.
-        /// </summary>
-        /// <typeparam name="T">Type of elements in the data Span.</typeparam>
-        /// <param name="destinationStartIndex">The first index in the destination buffer you want to copy data to</param>
-        /// <param name="data">Data array to be passed to the shader as a Span.</param>
-        /// elementCount will be inferred to be the number of elements in <paramref name="data"/>
-        /// since the Span should only contain the relevant data to be copied.
-        /// <remarks>
-        /// If <c>T</c> is <see cref="VertexPositionTexture"/>, and you want to only update the first 10 elements of your array of
-        /// <see cref="VertexPositionTexture"/>s, you would generate a Span containing those elements and pass it in
-        /// <code>
-        /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 0, 10);
-        /// vertexBuffer.SetData(0, vptSpan);
-        /// </code>
-        /// 
-        /// If you wanted to update the next 10 elements (indicies 10-19) in the source array, you would simply update the start index
-        /// <code>
-        /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 10, 10);
-        /// vertexBuffer.SetData(10, vptSpan);
-        /// </code>
-        /// <para>
-        /// Since a Span is a wrapper around a contiguous region of arbitrary memory, this is intended for cases with a 
-        /// vertexStride of <c>sizeof(T)</c>, as you need to generate a contiguous array of only relevant elements to populate
-        /// the Span, and the extra allocation and pre-processing to generate the Span partial objects will likely outweigh 
-        /// any benefits of passing a Span instead of a copy of the source data array.
-        /// </para>
-        /// </remarks>
-        public void SetData<T>(int destinationStartIndex, Span<T> data) where T : struct
-        {
-            SetDataInternal<T>(destinationStartIndex, data, data.Length, SetDataOptions.None);
-        }
-
-        /// <summary>
-        /// Sets the vertex buffer data. This is the same as calling <see cref="SetData{T}(int, Span{T})"/>
-        /// with <c>destinationStartIndex</c> equal to <c>0</c>
-        /// </summary>
-        /// <typeparam name="T">Type of elements in the data array.</typeparam>
-        /// <param name="data">Data Span to be passed to the shader.</param>
-        /// <inheritdoc cref="SetData{T}(int, Span{T})" path="/remarks"/>
-        public void SetData<T>(Span<T> data) where T : struct
-        {
-            var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
-            SetDataInternal<T>(0, data, data.Length, SetDataOptions.None);
-        }
-
-        /// <summary/>
-        protected void SetDataInternal<T>(int destinationStartIndex, Span<T> data, int elementCount, SetDataOptions options) where T : struct
-        {
-            if (data == null)
-                throw new ArgumentNullException("data");
-
-            var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
-            var offsetInBytes = destinationStartIndex * elementSizeInBytes;
-            var bufferSize = VertexCount * VertexDeclaration.VertexStride;
-
-            if (elementCount > data.Length || elementCount <= 0)
-                throw new ArgumentOutOfRangeException("data", "The array specified in the data parameter is not the correct size for the amount of data requested.");
-            if (offsetInBytes + elementCount * VertexDeclaration.VertexStride > bufferSize)
-                throw new ArgumentOutOfRangeException("The provided offset and data Span must total to a larger number of bytes than the vertex buffer");
-
-            PlatformSetData<T>(offsetInBytes, data, elementCount, VertexDeclaration.VertexStride, options, bufferSize, elementSizeInBytes);
-        }
-#endif
-
     }
 }

MonoGame.Framework/Platform/Native/VertexBuffer.Native.cs
@@ -14,6 +14,71 @@ public partial class VertexBuffer
 {
     internal unsafe MGG_Buffer* Handle;
 
+    /// <summary>
+    /// Sets the vertex buffer data, uses a Span including only relevant data to be copied rather than the full source array,
+    /// and the first index in the buffer to start copying to. Assumes the full Span will be copied with no padding between elements.
+    /// </summary>
+    /// <typeparam name="T">Type of elements in the data Span.</typeparam>
+    /// <param name="destinationStartIndex">The first index in the destination buffer you want to copy data to</param>
+    /// <param name="data">Data array to be passed to the shader as a Span.</param>
+    /// elementCount will be inferred to be the number of elements in <paramref name="data"/>
+    /// since the Span should only contain the relevant data to be copied.
+    /// <remarks>
+    /// If <c>T</c> is <see cref="VertexPositionTexture"/>, and you want to only update the first 10 elements of your array of
+    /// <see cref="VertexPositionTexture"/>s, you would generate a Span containing those elements and pass it in
+    /// <code>
+    /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 0, 10);
+    /// vertexBuffer.SetData(0, vptSpan);
+    /// </code>
+    /// 
+    /// If you wanted to update the next 10 elements (indicies 10-19) in the source array, you would simply update the start index
+    /// <code>
+    /// Span&lt;VertexPositionTexture&gt; vptSpan = new Span&lt;VertexPositionTexture&gt;(vptArray, 10, 10);
+    /// vertexBuffer.SetData(10, vptSpan);
+    /// </code>
+    /// <para>
+    /// Since a Span is a wrapper around a contiguous region of arbitrary memory, this is intended for cases with a 
+    /// vertexStride of <c>sizeof(T)</c>, as you need to generate a contiguous array of only relevant elements to populate
+    /// the Span, and the extra allocation and pre-processing to generate the Span partial objects will likely outweigh 
+    /// any benefits of passing a Span instead of a copy of the source data array.
+    /// </para>
+    /// </remarks>
+    public void SetData<T>(int destinationStartIndex, Span<T> data) where T : struct
+    {
+        SetDataInternal<T>(destinationStartIndex, data, data.Length, SetDataOptions.None);
+    }
+
+    /// <summary>
+    /// Sets the vertex buffer data. This is the same as calling <see cref="SetData{T}(int, Span{T})"/>
+    /// with <c>destinationStartIndex</c> equal to <c>0</c>
+    /// </summary>
+    /// <typeparam name="T">Type of elements in the data array.</typeparam>
+    /// <param name="data">Data Span to be passed to the shader.</param>
+    /// <inheritdoc cref="SetData{T}(int, Span{T})" path="/remarks"/>
+    public void SetData<T>(Span<T> data) where T : struct
+    {
+        var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
+        SetDataInternal<T>(0, data, data.Length, SetDataOptions.None);
+    }
+
+    /// <summary/>
+    protected void SetDataInternal<T>(int destinationStartIndex, Span<T> data, int elementCount, SetDataOptions options) where T : struct
+    {
+        if (data == null)
+            throw new ArgumentNullException("data");
+
+        var elementSizeInBytes = ReflectionHelpers.FastSizeOf<T>();
+        var offsetInBytes = destinationStartIndex * elementSizeInBytes;
+        var bufferSize = VertexCount * VertexDeclaration.VertexStride;
+
+        if (elementCount > data.Length || elementCount <= 0)
+            throw new ArgumentOutOfRangeException("data", "The array specified in the data parameter is not the correct size for the amount of data requested.");
+        if (offsetInBytes + elementCount * VertexDeclaration.VertexStride > bufferSize)
+            throw new ArgumentOutOfRangeException("The provided offset and data Span must total to a larger number of bytes than the vertex buffer");
+
+        PlatformSetData<T>(offsetInBytes, data, elementCount, VertexDeclaration.VertexStride, options, bufferSize, elementSizeInBytes);
+    }
+
     private unsafe void PlatformConstruct()
     {
         Handle = MGG.Buffer_Create(GraphicsDevice.Handle, BufferType.Vertex, _isDynamic, VertexCount * VertexDeclaration.VertexStride);